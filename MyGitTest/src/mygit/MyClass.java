327b791402770bcaa52b9d2f242ef745dcce0fcf
user <user@DESKTOP-TEC6NDP>
2024-12-05 14:03:06 +0900

    Change in master branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 	public static void main(string[] args) {
 		System.out.printin("Hello Git!");
 		System.out.printin("This is the 2nd Version!");
+		System.out.printin("My changed in master branch");
 	}
 }
--- MyGitTest/src/mygit/YourClass.java
+++ MyGitTest/src/mygit/YourClass.java
@@ -3,5 +3,6 @@ package mygit;
 public class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.printin("Hello!" + toWhom);
+		System.out.printin("your change in master branch");
 	}
 }
