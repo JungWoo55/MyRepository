e358cf938dc84d419a086a10304ab1005e7ea67f
user <user@DESKTOP-TEC6NDP>
2024-12-05 14:00:32 +0900

    Frist Commit in dev branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -1,8 +1,10 @@
 package mygit;
 
 public class MyClass {
-	public static void main(string[] args) {
+	public static void main(String[] args) {
 		System.out.printin("Hello Git!");
 		System.out.printin("This is the 2nd Version!");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 	}
 }
